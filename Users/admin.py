e08c0555a4b821315ccddc29150e7dcf64eb68e7
jurdin32 <urdin-23@live.com>
2020-07-26 18:15:46 -0500

    Cambio en el Modulo de Usuarios

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+from Users.models import *
+from ecShop.snippers import Attr
+
+class inLineTelefonos(admin.StackedInline):
+    model = Telefonos
+    extra = 0
+
+class inLineDirecciones(admin.StackedInline):
+    model = Direcciones
+    extra = 0
+
+class AdminClientes(admin.ModelAdmin):
+    list_display = Attr(Clientes)
+    list_display_links = Attr(Clientes)
+    inlines = [inLineDirecciones,inLineTelefonos]
+admin.site.register(Clientes,AdminClientes)
+
+class AdminPagos(admin.ModelAdmin):
+    list_display = Attr(Pagos)
+    list_display_links = Attr(Pagos)
+admin.site.register(Pagos,AdminPagos)
+
+class AdminListaDeseos(admin.ModelAdmin):
+    list_display = Attr(ListaDeseos)
+    list_display_links = Attr(ListaDeseos)
+admin.site.register(ListaDeseos,AdminListaDeseos)
+
+class AdminCarrito(admin.ModelAdmin):
+    list_display = Attr(Carrito)
+    list_display_links = Attr(Carrito)
+admin.site.register(Carrito, AdminCarrito)
+
+class AdminCompras(admin.ModelAdmin):
+    list_display_links = Attr(Compras)
+    list_display = Attr(Compras)
+admin.site.register(Compras,AdminCompras)
+
+class AdminDetalleCarrito(admin.ModelAdmin):
+    list_display = Attr(DetallesCarrito)
+    list_display_links = Attr(DetallesCarrito)
+admin.site.register(DetallesCarrito,AdminDetalleCarrito)
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,87 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from Administracion.models import Ciudad
+from Tienda.models import Productos
+
+
+class Clientes(models.Model):
+    nombres=models.CharField(max_length=60)
+    apellidos=models.CharField(max_length=60)
+    fecha_nacimiento=models.DateField(null=True,blank=True)
+    cedula=models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name="Cliente"
+        verbose_name_plural="Clientes"
+
+class Pagos(models.Model):
+    numero_tarjeta=models.CharField(max_length=20)
+    codigo_cvc=models.CharField(max_length=10)
+    principal=models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name="Pago"
+        verbose_name_plural="Pagos"
+
+class Telefonos(models.Model):
+    cliente=models.ForeignKey(Clientes,on_delete=models.CASCADE)
+    numero=models.CharField(max_length=10)
+    principal=models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name="Telefono"
+        verbose_name_plural="Telefonos"
+
+class Direcciones(models.Model):
+    cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    direccion=models.CharField(max_length=100)
+    ciudad=models.ForeignKey(Ciudad,on_delete=models.CASCADE)
+    descripcion=models.TextField(null=True,blank=True)
+    principal = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name="Direccion"
+        verbose_name_plural="Direcciones"
+
+class ListaDeseos(models.Model):
+    fecha=models.DateField(auto_now_add=True)
+    usuario=models.ForeignKey(User,on_delete=models.CASCADE)
+    producto=models.ForeignKey(Productos,on_delete=models.CASCADE)
+    slug=models.CharField(max_length=300,null=True,blank=True)
+
+    class Meta:
+        verbose_name="Lista de Deseos"
+        verbose_name_plural="Lista de Deseos"
+
+class Carrito(models.Model):
+    fecha=models.DateField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=9, decimal_places=2,default=0)
+    slug = models.CharField(max_length=300, null=True, blank=True)
+
+    class Meta:
+        verbose_name="Carrito"
+        verbose_name_plural="Carritos"
+
+class DetallesCarrito(models.Model):
+    fecha = models.DateField(auto_now_add=True)
+    producto = models.ForeignKey(Productos, on_delete=models.CASCADE)
+    cantidad = models.ImageField(default=0)
+    total = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+
+    class Meta:
+        verbose_name="Detalle Carrito"
+        verbose_name_plural="Detalles del Carrito"
+
+
+class Compras(models.Model):
+    fecha = models.DateField(auto_now_add=True)
+    carrito=models.ForeignKey(Carrito,on_delete=models.CASCADE)
+    valor=models.DecimalField(max_digits=9, decimal_places=2,default=0)
+    slug=models.CharField(max_length=300,null=True,blank=True)
+
+    class Meta:
+        verbose_name="Compra"
+        verbose_name_plural="Compras"
