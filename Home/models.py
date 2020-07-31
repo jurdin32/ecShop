4eda3129f3911b16dbd74924288bbab1b2485810
jurdin32 <urdin-23@live.com>
2020-07-31 17:57:26 -0500

    30/07/2020

--- Home/models.py
+++ Home/models.py
@@ -13,10 +13,8 @@ class Slider(models.Model):
     imagen=models.ImageField(upload_to="slider")
     titulo=models.CharField(max_length=60)
     descripcion=models.TextField(null=True,blank=True)
-    valorPromocion=models.DecimalField(max_digits=9, decimal_places=2,default=0)
-    valorSinPromocion=models.DecimalField(max_digits=9, decimal_places=2, default=0)
+    imagen2=models.ImageField(upload_to="slider/img2",null=True,blank=True)
     mensajeBoton=models.CharField(max_length=20, default="Llévame allá",null=True,blank=True)
-    alCarrito=models.BooleanField(default=False)
     enlace=models.URLField(max_length=2000,null=True,blank=True)
     colorBoton = ColorField(default='#FF0000')
 
